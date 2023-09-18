2f1278a1ef8649b4fa63b4cd961b500cf543e216
Renan <renan.rg@hotmail.com.br>
2023-09-18 17:12:00 -0300

    Finalizando o diretorio core.

---
+++ core/configs.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings
+
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Settings(BaseSettings):
+    """
+    Configurações gerais usadas na aplicação
+    """
+
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = 'postgresql+asyncpg://renan:1533@localhost:5432/faculdade_fastapi'
+    DBBaseModel = declarative_base()
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
+
+
