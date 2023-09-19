e79bc49d14636919407eed1466283d7a1422cd1a
Renan <renan.rg@hotmail.com.br>
2023-09-19 13:56:44 -0300

    Criando Schemas

--- core/configs.py
+++ core/configs.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from pydantic_settings import BaseSettings
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,7 +12,7 @@ class Settings(BaseSettings):
 
     API_V1_STR: str = '/api/v1'
     DB_URL: str = 'postgresql+asyncpg://renan:1533@localhost:5432/faculdade_fastapi'
-    DBBaseModel = declarative_base()
+    DBBaseModel: Any = declarative_base()
 
     class Config:
         case_sensitive = True
